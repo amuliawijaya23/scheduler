2bec21957479ab1e917cae8a9dad93e6c525cbaa
amuliawijaya23 <amuliawijaya@gmail.com>
2022-04-27 17:10:48 -0700

    refactor navigation testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,8 +1,13 @@
-const { CYCLIC_KEY } = require("@storybook/addon-actions")
 
 describe('Navigation', () => {
 
   it("should visit root", () => {
     cy.visit('/');
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit('/');
+    cy.contains('[data-testid=day]','Tuesday').click()
+      .should('have.class', 'day-list__item--selected');
   });
 });
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -11,7 +11,7 @@ export default function DayListItem(props) {
   });
 
   return (
-    <li onClick={props.setDay} className={dayClass}>
+    <li data-testid={'day'} onClick={props.setDay} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text-light">
         {props.spots > 0 && <>{props.spots} {(props.spots > 1) ? 'spots' : 'spot'} remaining</>}
